c1d725278d78f915de135ad48c0bf00416b22273
arya-asa-fikarda <aryaasafikarda08@gmail.com>
2026-03-30 14:13:35 +0700

    cp20

--- resources/views/admin/pages/gallery/create.blade.php
+++ resources/views/admin/pages/gallery/create.blade.php
@@ -5,9 +5,7 @@
         <div class="col-12">
             <div class="card">
 
-                <div class="card-header">
-                    <h3 class="card-title"><b>Tambah Gallery</b></h3>
-                </div>
+               
 
                 <div class="card-body">
 
@@ -22,12 +20,13 @@
 
                         <div class="form-group">
                             <label>Category</label>
-                            <select name="category" class="form-control">
-                                <option value="">-- Pilih Category --</option>
-                                <option value="pergedungan">Pergedungan</option>
+                            <select name="category" class="form-control" required>
+                                <option value="">--Pilih Opsi--</option>
+                                <option value="pergedungan">Pergedugan</option>
                                 <option value="prestasi">Prestasi</option>
                                 <option value="acara">Acara</option>
                             </select>
+
                         </div>
 
                         <div class="form-group">
