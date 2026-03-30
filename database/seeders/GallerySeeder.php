044cf488b56c37a4525d50c3565727b280c2e780
aqeel <haikalrahman69612@example.com>
2026-03-30 13:32:56 +0700

    cp18

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -17,26 +17,32 @@ class GallerySeeder extends Seeder
             [
                 'title' => 'Gedung Rektorat (PAU) UIN Sunan Kalijaga',
                 'image' => 'gallery-pau.jpg',
+                'category' => 'pergedungan',
             ],
             [
                 'title' => 'Perpustakaan Ikonik Berstandar Internasional',
                 'image' => 'gallery-library.jpg',
+                'category'=> 'pergedungan',
             ],
             [
                 'title' => 'Laboratorium Terpadu Integrasi-Interkoneksi',
                 'image' => 'gallery-lab.jpg',
+                'category'=> 'pergedungan',
             ],
             [
                 'title' => 'Momen Wisuda di Gedung Multi Purpose',
                 'image' => 'gallery-graduation.jpg',
+                'category'=> 'prestasi',
             ],
             [
                 'title' => 'Fasilitas Ramah Difabel - Kampus Inklusif',
                 'image' => 'gallery-pld.jpg',
+                'category'=> 'pergedungan',
             ],
             [
                 'title' => 'Masjid Laboratorium Agama Sunan Kalijaga',
                 'image' => 'gallery-mosque.jpg',
+                'category'=> 'pergedungan',
             ],
         ];
 
